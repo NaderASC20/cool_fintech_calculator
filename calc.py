cbe4cf7e3e9b5a98d031a8024d0910fa53ace2a7
Nader Daruvala <ndaruvala2346@gmail.com>
2021-07-08 19:10:01 +0000

    Finished project?

--- calc.py
+++ calc.py
@@ -1,32 +1,37 @@
-# Restaurant Tip Calculator
 # Created by Nader Daruvala and Tia Hannah
+# Restaurant Tip Calculator
 
 print('-' * 50)
 total = float(input("What is the total bill cost? ")) # Total cost of the bill
-percent = int(input("What is your desired tip percent? (exclude % symbol please!) ")) / 100 # Percent tip as a decimal, ex. .20 = 20%
-people = int(input("How many people were seated at your table? (Including you) ")) # Number of people at the table
-server = int(input("How would you rate your server out of 5")) 
-print('-' * 50 + "\n\n\n")
 
+people = int(input("How many people were seated at your table? (Including you)?  ")) # Number of people at the table
+while people < 1:
+    people = int(input("Please include yourself! Try again. How many people were at your table? ")) # Number of people at the table
+
+server = int(input("How would you rate your server out of 5? (1 - 5 only) "))
+while server < 1 or server > 5:
+    server = int(input("Your input was not valid! Try again. How would you rate your server (1 - 5 only) ")) # Number of people at the table
+
+# Percent = # Function call
+print('-' * 50 + "\n\n")
 # Simple calculator
-def tip_simple(total, percent = .15, people = 1):
-    tip = total * percent
+
+
+def tip_calc(total, server, people):
+    tip = server_tip(total, server)
     split = tip / people
-    return ("=" * 50) + "\n" + f"You're total tip is: ${tip}".center(50) + "\n" + f"That is equal to ${split} per person!".center(50) + "\n" + ("=" * 50)
+    cost = total + tip
+    return ("=" * 50) + "\n" + f'Total cost: ${cost}'.center(50) + "\n" + f"Total tip: ${tip}".center(50) + "\n" + f"Each person pays ${split} for tip!".center(50) + "\n" + ("=" * 50) + "\n"
 
-# 5 = 25%  / 4 = 20%  / 3 = 15%  / 2 = 10% / 1 = 5%
+def server_tip(total, server):
+    ratings = {
+        5 : .25 * total,
+        4 : .20 * total,
+        3 : .15 * total,
+        2 : .10 * total,
+        1 : .05 * total
+    }
 
-def server_tip(server):
-    if server == 5:
-        return total * .25
-    elif server == 4:
-        return total  
-    elif server == 3:
-        return total 
-    elif server == 2
-        return total 
-    elif server == 1 
-        return total    
-   
-    
-print(tip_simple(total, percent, people))
+    return ratings[server]
+
+print(tip_calc(total,server,people))
